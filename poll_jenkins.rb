ffd2b8423515e93b18b1fcf1dd64583499b5b682
Johannes Brodwall <johannes@brodwall.com>
2011-02-18 08:48:57 -0800

    Now support red lights, restore status after error and added exit hook.

--- poll_jenkins.rb
+++ poll_jenkins.rb
@@ -3,13 +3,17 @@ require 'open-uri'
 require 'rexml/document'
 
 # Usage
-#  poll_resource [<jenkins-url>|http://localhost:8080/job/java-ee-spike-kata] [<led number>] [<polling frequency>]
+#  poll_resource [<jenkins-url>|http://localhost:8080/job/foo] [<led number>] [<polling frequency>]
 #    url: a file or http url that should be polled every five seconds
 #    led number: 0...x where x is the number of LEDs connected to this computer
 #    polling frequency: The number of seconds between checking the URL
 
 $led = VisualIndicator.all[ARGV[1] || 0]
-status_url = "#{ARGV[0] || 'http://localhost:8080/job/java-ee-spike-kata'}/api/xml"
+status_url = "#{ARGV[0] || 'http://localhost:8080/job/foo'}/api/xml"
+
+END {
+  $led.all.off
+}
 
 $status = :none
 
@@ -17,11 +21,14 @@ def status(new_status)
   return if new_status == $status
   $status = new_status
   case $status
-    when :ok then $led.all.off; $led.green.on 5
-    when :build_from_ok then $led.all.off; $led.green.flash 50,50,5
-    when :fail then $led.all.off; $led.yellow.on 5
-    when :build_from_fail then $led.all.off; $led.yellow.flash 50,50,5
-    else $led.all.off; $led.red.flash 5, 5, 20
+    when :ok then $led.all.off; $led.green.on 100
+    when :build_from_ok then $led.all.off; $led.green.flash 50,50,100
+    when :fail then $led.all.off; $led.yellow.on 100
+    when :build_from_fail then $led.all.off; $led.yellow.flash 50,50,100
+    when :error then $led.all.off; $led.red.on 100
+    when :build_from_error then $led.all.off; $led.red.flash 50,50,100
+    when :system_error then $led.all.off; $led.red.flash 10, 10, 100
+    else $led.all.off; $led.red.flash 5, 5, 100
   end
 end
 
@@ -37,13 +44,14 @@ while true
         when "blue_anime" then status(:build_from_ok)
         when "yellow" then status(:fail)
         when "yellow_anime" then status(:build_from_fail)
+        when "red" then status(:error)
+        when "red_anime" then status(:build_from_error)
         else status(:unknown)
       end
     end
   rescue => e
     puts "Could not access #{ARGV[0]}: #{e.message}"
-    $led.all.off
-    $led.red.flash 10, 10, 20
+    status(:system_error)
   end
   sleep (ARGV[2] || "1").to_i
 end
